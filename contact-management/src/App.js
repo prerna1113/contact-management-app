0b7a70f22d2304ad75910b62cd375659bd005e21
Pre-11 <prernakumari7427@gmail.com>
2023-05-02 13:58:56 +0530

    added

--- contact-management/src/App.js
+++ contact-management/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 // import Input from './Component/Input';
 // import Contact from './Component/Contact';
 import Nav from './Component/Nav';
+import { Route,Routes } from 'react-router-dom'
+import Contact from './Component/Contact';
+import Maps from './Component/Maps';
+import Sidebar from './Component/Sidebar';
 
 
 
@@ -12,6 +16,12 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Routes>
+      <Route path="/" element={<Contact />}/>
+      <Route path="/maps and Charts" element={<Maps />}/>
+      <Route  path="/sidebar" element ={<Sidebar />}/>
+      </Routes>
+     
     {/* <Input /> */}
     {/* <Contact /> */}
     
--- contact-management/src/Component/Nav.js
+++ contact-management/src/Component/Nav.js
@@ -3,12 +3,19 @@ import {Link} from 'react-router-dom'
 
 function Nav() {
   return (
-    <div style={{display:"flex",margin:"auto",padding:"20px",width:"30%",height:"200px",
-    border:"1px solid black",marginTop:"30px"
+   <div>
+    <h1 style={{width:"33%",margin:"auto",border:"1px solid black"
+    ,marginBottom:"0px",color:"black",backgroundColor:"cyan"
+    }}>Contact Page</h1>
+
+
+<div style={{display:"flex",margin:"auto",width:"33%",height:"200px",
+    border:"1px solid black",
     }}>
-        <div style={{border:"2px solid black",width:"25%"}}>
-            <br></br>
-            <Link to="/">Contact</Link>
+    
+        <div style={{width:"25%",boxShadow:"0 0 10px black",}}>
+           <br></br>
+            <Link  to="/">Contact</Link>
             <br></br>
             <br></br>
             
@@ -18,11 +25,12 @@ function Nav() {
             <Link to ="sidebar">Sidebar</Link>
             </div>
 
-            <div style={{border:"2px solid red",width:"75%"}}>
-                <h1>Contact Screen</h1>
+            <div style={{width:"75%",marginTop:"2px"}}>
+                <h3>Contact Screen</h3>
             </div>
        
     </div>
+   </div>
   )
 }
 
